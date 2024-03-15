0d0f8f612c0bedd1d5564078a7567bfba8cfa346
Nils Kleine <nils.kleine@volkswagen.de>
2024-03-15 14:41:13 +0100

    added components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       HELLO WORLD!
         <Link href={"/recipes"}>Recipes</Link>
     </main>
---
+++ frontend/src/common/nav-bar.tsx
@@ -0,0 +1,8 @@
+const navItems = ["Home", "Recipes"]
+export default function NavBar() {
+    const navBar =
+        <>
+
+        </>
+    return(navBar)
+}
